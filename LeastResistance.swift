824992726f03382bad4b139401fb0a624523eca6
Jonathan Moody <jonmoody12@gmail.com>
2016-04-07 12:00:17 -0400

    Created helper methods for row and column validation

--- LeastResistance.swift
+++ LeastResistance.swift
@@ -7,6 +7,14 @@ class LeastResistance {
     }
 
     func isValidGrid() -> Bool {
-        return grid.count > 0 && grid.count <= 10 && grid[0].count >= 5 && grid[0].count <= 100
+        return gridContainsValidNumberOfRows() && gridContainsValidNumberOfColumns()
+    }
+
+    func gridContainsValidNumberOfRows() -> Bool {
+        return grid.count > 0 && grid.count <= 10
+    }
+
+    func gridContainsValidNumberOfColumns() -> Bool {
+        return grid[0].count >= 5 && grid[0].count <= 100
     }
 }
